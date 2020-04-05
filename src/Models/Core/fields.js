b26a18c26567481794a4fb7ff20fdbe087a3a301
teadams <tracyeadams@gmail.com>
2020-04-05 14:32:55 -0400

    Featuer: Core model for subsite, roles, subsite_roles, Context subsite map

--- src/Models/Core/fields.js
+++ src/Models/Core/fields.js
@@ -1,10 +1,35 @@
 
 const metadata_fields = {
   // fields that are added to every table
+  // fields that are added to every table
   core_fields: [
-    {name:"creation_date", pretty_name:"Creation Date", data_type: "timestamp", default:"now()", required:true, index:true, system_controlled:true},
-    {name:"last_updated_date", pretty_name:"Last Updated Date", data_type: "timestamp", default:"now()", required:true, index:true, system_controlled:true},
-    {name:"creation_user", pretty_name:"Creation User", data_type:"integer", references:"core_user", use_context:true, system_controlled:true}
+    {name:"creation_date", pretty_name:"Creation Date", data_type: "timestamp", default:"now()", required:true, index:true},
+    {name:"last_updated_date", pretty_name:"Last Updated Date", data_type: "timestamp", default:"now()", required:true, index:true},
+    {name:"creation_user", pretty_name:"Creation User", data_type:"integer", references:"core_user"},
+    // later - technically only needed for objects that ues subsites
+    {name:"core_subsite", pretty_name:"Core Subsite", data_type:"integer", references:"core_subsite"}
+  ],
+  // subsites are within a tenant. Tables are striped with the subsite_id
+  // Roles will be linked to subsite
+ // later: limit to 3 levels for now.  Data model will support infinite, but keep things simple
+  core_subsite: [
+    {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
+    {name:"name", pretty_name: "Subsite", data_type: "string", pretty_key:true, index:true},
+    // level is a denormlization as you could technically figure it out from core_subsite_hierarchy
+    // top level is 1. 
+    {name:"level", pretty_name: "Level", data_type: "integer", start_value:1, end_value:3, index:true}
+  ],
+  // denormalization
+  // this one row for each subsite, pluse one row for each subsite in it's heirarcy
+  // used for efficients queries like "if I have admin on subsite 1, what other subsites to I have admin on"
+  core_subsite_hierarchy: [
+    {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
+    {name:"core_subsite", pretty_name:"Core Subsite", data_type:"integer", references:"core_subsite", index:true},
+    // named ancestor, but we also store the subsite itself with levels_removed = 0. 
+    // done to make queries like "show me everything you have access to efficient"
+    {name:"ancestor", pretty_name:"Ancestor", data_type:"integer", references:"core_subsite", index:true},
+    // 0 if core_subsite and ancestor are the same (the leaft subsite)
+    {name:"generations_removed", pretty_name: "Level", data_type: "integer", index:true}
   ],
   core_country: [
     {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
@@ -34,7 +59,28 @@ const metadata_fields = {
   core_site_admin: [
     {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
     {name:"core_user", pretty_name:"Site Administrator Name", references:"core_user", index:true, pretty_key:true, prevent_edit:true}
-  ]
+  ], 
+  core_role: [
+    {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
+    {name:"name", pretty_name:"Role Name", data_type:"string", index:true, pretty_key:true}
+  ],
+  core_subsite_role: [
+    {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
+    {name:"core_user", pretty_name:"Subsite Administrator Name", references:"core_user", index:true, pretty_key:true},
+    {name:"core_subsite", pretty_name:"Subsite", references:"core_subsite", index:true},
+    {name:"core_role", pretty_name:"Role",data_type:"string", references:"core_role", index:true}
+  ],
+  // one object type will be designated as context (and used to drive the user interface)
+  // this object my be linked to a context
+  core_context_subsite_map: [
+    {name:"id", pretty_name: "Id", data_type: "integer", required:true, key:true},
+    // id of a row from the context object_type
+    // general use case / application code will have 1-to-1 object to subsite
+    // however, the data model gives flexibity to support mony-to-1
+    {name:"core_context", pretty_name:"Context", data_type:"integer", index:true, pretty_key:true},
+    {name:"core_subsite", pretty_name:"Subsite", references:"core_subsite", index:true}
+  ],
+
 }
 
 //module.exports = {
--- src/Models/Core/object_types.js
+++ src/Models/Core/object_types.js
@@ -1,13 +1,16 @@
 const metadata_object_types = [
-  {name:"core_fields", pretty_name:"Core Field", pretty_plural:"Core Fields"},
-  {name:"core_country", pretty_name:"Country", pretty_plural:"Countries"},
-  {name:"core_state_province", pretty_name:"State or Province", pretty_plural:"States/Provinces"},
-  {name:"core_user", pretty_name:"User", pretty_plural:"Users"},
-  {name:"core_site_admin", pretty_name:"Site Admin", pretty_plural:"Site Admins"},
-  {name:"core_credential", pretty_name:"Core Credential", pretty_plural:"Core Credentials"},
+  {name:"core_fields", pretty_name:"Core Field", pretty_plural:"Core Fields", all_subsites:true, not_in_db:true},
+  {name:"core_subsite", pretty_name:"Core Subsite", pretty_plural:"Core Subsites", all_subsites:true},
+  {name:"core_country", pretty_name:"Country", pretty_plural:"Countries", all_subsites:true},
+  {name:"core_state_province", pretty_name:"State or Province", pretty_plural:"States/Provinces", all_subsites:true},
+  {name:"core_user", pretty_name:"User", pretty_plural:"Users", all_subsites:true},
+  {name:"core_site_admin", pretty_name:"Site Admin", pretty_plural:"Site Admins", all_subsites:true},
+  {name:"core_credential", pretty_name:"Core Credential", pretty_plural:"Core Credentials", all_subsites:true},
+  {name:"core_role", pretty_name:"Role", pretty_plural:"Roles", all_subsites:true},
+  {name:"core_subsite_role", pretty_name:"Subsite Role", pretty_plural:"Subsite Roles", all_subsites:true},
+  {name:"core_context_subsite_map", pretty_name:"Context to Subsite Link", pretty_plural:"Context to Subsite Links", all_subsites:true}
 ]
 
-//export default metadata_object_types
-
 export default  metadata_object_types
+
 
