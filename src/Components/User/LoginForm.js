8bcbfc2312e7ed1800eb2cd859edec74bb3916de
teadams <tracyeadams@gmail.com>
2020-04-13 19:52:47 -0400

    feature: Adding user's context privileges to context authorization (login) object

--- src/Components/User/LoginForm.js
+++ src/Components/User/LoginForm.js
@@ -57,6 +57,7 @@ class LoginForm extends React.Component {
         } else if (!user_data.password_match) {
             alert ("password is not correct")
         } else {
+          alert ("user data is "  + JSON.stringify(user_data))
           this.context.login(user_data)  
         }
     })
