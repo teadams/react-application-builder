500bb8e7afe60dd93239077df33fff11d998df9f
teadams <tracyeadams@gmail.com>
2020-04-13 21:42:40 -0400

    Feature: Workflow for nwa_core_subsite_role based on status column
    Use subsite_member_role_statuses in app.js to specify which statuses 
    will qualify for membership.  For example, "Approved" might be a valid 
    status with Applied, Rejected, and Invalid as non-valid statuses.

--- src/Models/NowWeAct/app.js
+++ src/Models/NowWeAct/app.js
@@ -1,9 +1,10 @@
 const app_params =    {
   name:"Now We Act",
   prefix:"nwa",
-  context_object_tpye:"nwa_project",
   context_default_object:"2" , // must be a string
-  footer: ""
+  footer: "",
+  subsite_member_role_statuses: ["Approved"]
+
 }
 
 export default app_params
