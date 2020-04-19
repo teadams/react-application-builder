7b562a818eee7c5d39279031a37f18404ee8b048
teadams <tracyeadams@gmail.com>
2020-04-19 14:12:50 -0400

    Feature: Beatiful Debug component using hooks

--- src/Debug.js
+++ src/Debug.js
@@ -1,19 +1,107 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
+import * as data from './Utils/data.js';
 
-import React, { Component, Fragment, useState, useContext} from 'react';
+import React, { Component, Fragment, useState, useContext, useEffect} from 'react';
 import {AuthContext} from './Components/User';
-import Container from '@material-ui/core/Container'
-import {Snackbar, SnackbarContent, Button, Grid} from '@material-ui/core';
+import {Container, Box, Grid, Typography, TableContainer, Table, TableBody, TableRow, TableCell} from '@material-ui/core';
 
+const container_style = { 
+  padding:20,
+  backgroundColor:"lightGray",
+  borderColor:"darkGray",
+  borderWidth:"thick",
+  borderStyle:"solid"
+}
+const title_style = {
+  color:"red"
+}
+const grid_item_style = {
+  padding:10
+}
 
+function UserInfo () {
+    const context = useContext(AuthContext)
+    const user_logged_id = context.user && context.user.id?true:false
+    if (user_logged_id)  {
+        return context.user.first_name + context.user.last_name
+   } else {
+      return "Not logged in"
+  }
+}
+
+function UserSiteAdmin () {
+    const context = useContext(AuthContext)
+    const user_logged_id = context.user && context.user.id?true:false
+    if (user_logged_id)  {
+        return context.user.first_name + context.user.last_name
+   } else {
+      return "Not logged in"
+  }
+}
+
+function Context () {
+  const [subsite_data, setSubsiteData] = useState({});
+  const context = useContext(AuthContext)
+  useEffect(() => {
+      data.getData ("core_subsite", {id:context.context_id}, (results, error) => { 
+          if (!error) {
+              setSubsiteData(results)
+          }
+      })
+}, [context.context_id]);
+  return `${context.context_id}  ${subsite_data.name?` - ${subsite_data.name}`:""}`
+}
+
+function UserSubsiteAuthRoles () {
+    const context = useContext(AuthContext)
+    const subsite_id = context.context_id
+    if (context.user && context.user.authorization_object[subsite_id] ) {
+      return JSON.stringify(context.user.authorization_object[subsite_id].Roles)
+    } else {
+      return ""
+    }
+}
+
+function UserSubsiteAuthPriv () {
+  const context = useContext(AuthContext)
+  const subsite_id = context.context_id
+  if (context.user && context.user.authorization_object[subsite_id] ) {
+    return JSON.stringify(context.user.authorization_object[subsite_id].Privileges)
+  } else {
+    return ""
+  }
+}
 
 function Debug(props) {
   const context = useContext(AuthContext)
   return (
-      <Container maxWidth="sm">
-      THIS IS THE DEBUG COMPONENT and user is
-      {context.user.id}
+      <Container maxWidth="sm" style={container_style}>
+        <Typography variant="h5" style={title_style}>Debug Information</Typography>
+        <TableContainer>
+          <Table>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell><UserInfo/></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Site Administrator</TableCell>
+              <TableCell><UserSiteAdmin/></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Subsite</TableCell>
+              <TableCell><Context/></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Subsite Roles</TableCell>
+              <TableCell><UserSubsiteAuthRoles/></TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Subsite Privileges</TableCell>
+              <TableCell><UserSubsiteAuthPriv/></TableCell>
+            </TableRow>
+          </Table>
+        </TableContainer>
       </Container>
   )
 }
