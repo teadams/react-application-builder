f729fec9629bca65ed5383d85d0720a2e3d77125
teadams <tracyeadams@gmail.com>
2020-04-21 13:38:20 -0400

    Custome: Set up HealthMe models

---
+++ src/Models/HealthMe/index.js
@@ -0,0 +1,6 @@
+import metadata_menus from './menus';
+import metadata_sections from './sections'
+
+export  {
+    metadata_menus, metadata_sections
+}
---
+++ src/Models/HealthMe/menus.js
@@ -0,0 +1,10 @@
+const metadata_menus = {
+  app_menu: [
+    {index:"0", label: "Subsites", object_type: "core_subsite", component:"DrillDown"}
+ ],
+  hamburger: [
+    {index:"0", label: "Subsites", object_type: "core_subsite", component:"DrillDown"}
+  ]
+} 
+
+export default metadata_menus;
