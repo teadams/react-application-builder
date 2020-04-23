fcc4e0ea89838aabaa91fe08715277edbfc01f80
teadams <tracyeadams@gmail.com>
2020-04-22 22:03:27 -0400

    Feature; adding first use of TreeView, required material-ui lab

--- src/Functional/ObjectList.js
+++ src/Functional/ObjectList.js
@@ -6,13 +6,31 @@ import * as data from '../Utils/data.js';
 import { withStyles } from '@material-ui/core/styles';
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
 import useObjectListGet from '../Hooks/useObjectListGet';
-
 import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField
 , Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar } from '@material-ui/core';
 
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+
 function ObjectList(props) {
   const object_list_data = useObjectListGet(props.object_type);
-  return <Typography>congrats you are in the Object list</Typography>
+  return (<Fragment>
+  <TreeView
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      <TreeItem nodeId="1" label="Applications">
+        <TreeItem nodeId="2" label="Calendar" />
+        <TreeItem nodeId="3" label="Chrome" />
+        <TreeItem nodeId="4" label="Webstorm" />
+      </TreeItem>
+  </TreeView>
+  </Fragment>
+  );
 }
+
 
 export default ObjectList;
