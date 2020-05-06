f81cea939eeffda0c91ac8ce4555e5e375926059
teadams <tracyeadams@gmail.com>
2020-05-06 11:14:54 -0400

    completing list of dynamic Components we should be able to get rid of functiaonl_components once this is all refactored.

--- src/Utils/control.js
+++ src/Utils/control.js
@@ -1,3 +1,4 @@
+import {Fragment} from 'react';
 import ObjectView from "../RABComponents/ObjectView.js"
 import NavMenu from "../RABComponents/NavMenu.js"
 import DrillDown from "../RABComponents/DrillDown.js"
@@ -5,18 +6,61 @@ import FieldView from "../RABComponents/FieldView.js"
 import TreeMenu from "../RABComponents/TreeMenu.js"
 import Text from "../RABComponents/Text.js"
 import ObjectTypeView from "../RABComponents/ObjectTypeView.js"
+import Debug from '../Debug.js'
+import ACSField from '../Functional/ACSField2.js'
+import ACSRowController from '../Functional/ACSRowController.js'
+import ACSListController from '../Functional/ACSListController.js'
+import RenderACSField from '../Functional/RenderACSField.js'
+import RenderACSRow from '../Functional/RenderACSRow.js'
+import RenderACSList from '../Functional/RenderACSList.js'
+import {Tab, Tabs, Menu, MenuItem, MenuList,List,ListItem,ListItemAvatar,ListItemIcon,ListItemSecondaryAction,ListItemText,ListSubheader,Table,TableBody,TableCell,TableContainer,TableFooter,TableHead,TablePagination,TableRow,} from '@material-ui/core';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+
+
 
 
 
 export function componentByName(name) {
   switch (name) {
     case "ObjectView": return ObjectView;
-    case "Text":return Text
-    case "NavMenu":return NavMenu
-    case "DrillDown":return DrillDown
-    case "FieldView":return FieldView
-    case "TreeMenu":return TreeMenu
-    case "ObjectTypeView":return ObjectTypeView
+    case "Text": return Text;
+    case "NavMenu": return NavMenu;
+    case "DrillDown": return DrillDown;
+    case "FieldView": return FieldView;
+    case "TreeMenu": return TreeMenu;
+    case "ObjectTypeView": return ObjectTypeView;
+    case "Debug": return Debug;
+    case "ACSField": return ACSField;
+    case "ACSRowController": return ACSRowController;
+    case "ACSListController": return ACSListController;
+    case "RenderACSField": return RenderACSField;
+    case "RenderACSRow":  return RenderACSRow;
+    case "RenderACSList": return RenderACSList;
+    case "Tab": return Tab;
+    case "Tabs": return Tabs;
+    case "Menu": return Menu;
+    case "MenuItem": return MenuItem;
+    case "MenuList": return MenuList;
+    case "List": return List;
+    case "ListItem": return ListItem;
+    case "ListItemAvatar": return ListItemAvatar; 
+    case "ListItemIcon": return ListItemIcon;
+    case "ListItemSecondaryAction": return ListItemSecondaryAction; 
+    case "ListItemText": return ListItemText;
+    case "ListSubheader": return ListSubheader; 
+    case "Table": return Table; 
+    case "TableBody": return TableBody; 
+    case "TableCell": return TableCell; 
+    case "TableContainer": return TableContainer;
+    case "TableFooter": return TableFooter; 
+    case "TableHead": return TableHead;
+    case "TablePagination": return TablePagination; 
+    case "TableRow": return TableRow; 
+    case "TreeItem": return TreeItem; 
+    case "TreeView": return TreeView;
+    case "Fragment": return Fragment;
+    default:  return Text;
   }
 }
 
