164faf0959716c6ce266393028cee57250aee4e7
teadams <tracyeadams@gmail.com>
2020-05-06 11:20:02 -0400

    unneccessary imports

--- src/Body.js
+++ src/Body.js
@@ -2,20 +2,11 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
 import React, { Component, Fragment} from 'react';
-import {dynamic_components} from "./dynamic-components"
-import {AuthToggleLink, AuthContext, AuthProvider} from './Components/User';
 import * as meta from './Utils/meta.js'
 import * as log from './Utils/log.js'
 import * as u from './Utils/utils.js'
 import * as control from './Utils/control.js'
 import useGetModel from "./Hooks/useGetModel.js"
-
-import axios from 'axios';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import {  BrowserRouter as Router,  Switch,  Route,  Link,  useParams } from "react-router-dom";
 
 function Body(props) {
   const {selected_menu} = props
