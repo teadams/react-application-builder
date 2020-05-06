72c98c409044787ae564ae5ad3e1faea3154d95a
teadams <tracyeadams@gmail.com>
2020-05-06 10:45:31 -0400

    inital layout

---
+++ src/Models/HealthMe/component.js
@@ -0,0 +1,24 @@
+onst rab_component_model = {
+  shell: {
+    list: {
+      component_names:{},
+      components:{}
+      defaut_mode:"view",  
+      props:{}
+    },
+    row: {
+      component_names:{},
+      components:{}
+      default_mode:"view",
+      props:{}
+    },
+    field:{
+      component_names:{},
+      components:{}
+      default_mode:"click_to_edit",
+      props:{}
+    }
+  }
+}
+
+export default rab_component_model
