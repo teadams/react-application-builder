05e8618476415997425554f8a9439628e3d69820
teadams <tracyeadams@gmail.com>
2020-05-09 08:22:10 -0400

    refactoring to new model

--- src/Functional/ACSField2.js
+++ src/Functional/ACSField2.js
@@ -4,6 +4,9 @@ import * as u from '../Utils/utils.js';
 import * as meta from '../Utils/meta.js'
 
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
+
+import RenderACSField from './RenderACSField.js'
+
 import useGetObject from '../Hooks/useGetObject';
 import useGetModel from '../Hooks/useGetModel';
 
@@ -15,30 +18,7 @@ function ACSField(input_props) {
   const [mode, setMode] = useState("view");
   const field_models =  useGetModel("fields")
   let field_model = field_models?field_models[input_props.object_type][input_props.field_name]:{}
-//  if (!field_models) {return null} *** Breaks our rule, which depended on all the models being loaded.
-// not a priority now as we know the model is here. Will impact lazy loading of models.
 
-  // take care of fields that do not need to be merged
-  const {...merging_props} = input_props
-  const rab_component_model = control.getFinalModel("field", {...merging_props}, field_model, field_model.rab_component_model)
-  const component_field_model = rab_component_model.field
-  const component_field_components = component_field_model.components
-  const massaged_props = component_field_model.props
-
-  const {object_type:props_object_type, id:props_id, field_name:props_field_name, api_options:props_api_options, data:props_data, component, ...params} = massaged_props
-
-  let [ready, object_type, id, field_name, api_options, data] = useGetObject(props_object_type, props_id,props_field_name, props_api_options, props_data); 
-  if (!data || (object_type && !field_model)) return null
-
-// XX ?? look at rest of props and see if there are any other API options... what layer to do this in
-  function handleViewClick(event) {
-      setMode("edit")
-  }
-
-  function handleEditSubmit(event) {
-      setMode("view")
-  }
-    
   // Use case - this field has been tagged with "references"
   // which indicates the field is from another object type.
   // Need to modify that the  object_type, field_name to 
@@ -53,10 +33,37 @@ function ACSField(input_props) {
       referenced_field_model.pretty_name = field_model.pretty_name // take name from base object
       field_model = referenced_field_model
   }
-  // props, meta, default
+
+  const {...merging_props} = input_props
   
-  const RenderACSField = component_field_components.field
-  const ACSCell = component_field_components.field_wrap
+  const rab_component_model = control.getFinalModel("field", {...merging_props}, field_model, rab_component_models.field)
+
+  const field_component_model = rab_component_model.field
+  const massaged_props = field_component_model.props
+
+  const {object_type:props_object_type, id:props_id, field_name:props_field_name, api_options:props_api_options, data:props_data, component, ...params} = massaged_props
+
+  let [ready, object_type, id, field_name, api_options, data] = useGetObject(props_object_type, props_id,props_field_name, props_api_options, props_data); 
+  if (!data || (object_type && !field_model)) return null
+
+// Determine value
+// maybe have to get the select list values
+
+// Determine the final field component 
+// to used (if already defined, based)
+// on rules from the mdoel, Text)
+
+
+// XX ?? look at rest of props and see if there are any other API options... what layer to do this in
+  function handleViewClick(event) {
+      setMode("edit")
+  }
+
+  function handleEditSubmit(event) {
+      setMode("view")
+  }
+    
+// Determine the mode
 // state will track a view/edit mode
 // Use case
 // When user clicks on a field in view mode, it will
@@ -64,11 +71,9 @@ function ACSField(input_props) {
 // form, the form is submitted and the page returns
 // to view mode.  
 
-// I think the field will be responsible for the mode
 return (
-  <ACSCell {...component_field_model.props}  data={data} object_type={object_type} field_name={field_name} field_model={field_model}  rab_component_model={rab_component_model}>
-    <RenderACSField {...component_field_model.props}  data={data} object_type={object_type} field_name={field_name} field_model={field_model} rab_component_model={rab_component_model}/>
-  </ACSCell>
+    <RenderACSField {...field_component_model.props}  data={data} 
+    object_type={object_type} field_name={field_name} field_model={field_model} rab_component_model={rab_component_model}/>
   )
 }
 
