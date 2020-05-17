4d02f106f4be3bcd8598b2121b72cba0a4eddeb8
teadams <tracyeadams@gmail.com>
2020-05-16 23:28:59 -0400

    select feld

--- src/Functional/Fields/RABSelectField.js
+++ src/Functional/Fields/RABSelectField.js
@@ -18,52 +18,39 @@ import rab_component_models from '../../Models/HealthMe/component.js'
 //   helperText={field.helper_text}
 //   value=  {this.getDisplayView(object_type,field,prefix)}
 //  style={{width:"90%"}}
+function RABSelectObject(props) {
+  return (<FormControl>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      name={props.select_form_name}
+      value={props.value}
+      autoFocus={props.autoFocus}
+      onBlur={props.onSubmit}
+      onChange={props.onChange}
+    >{props.data.map (row => {
+      return (<MenuItem value={row[props.select_key_field]}>{row[props.select_display_field]}</MenuItem>)
+      })}
+    </Select>
+  </FormControl>)
+}
 
 function RABSelectField(props) {
-  const {mode, data, field_name, formdata, formValues,  onChange, autoFocus, onBlur, object_type, field_model} = props
-//  u.aa("in select, field_name", field_name, formValues)
+  const {mode, data, field_name, formdata, formValues, onSubmit, onFieldBlur,  onChange, autoFocus, object_type, field_model} = props
   const formValues_name = field_model.formValues_name
-//  const field_value = "foo"
   const field_value = formValues[formValues_name]
   const field_display_value = data[field_name]
+  const select_key_field = field_model.select_key_field
+  const select_display_field = field_model.select_display_field
   let rab_component_model = rab_component_models.shell
-
-  //u.a(formValues_name, field_value) // original name... reference field name
-  // object_type, dispaly_typex
-
-//    <TextField 
-//      autoFocus={autoFocus}
-//      name={field_name} 
-//      value={field_value}
-//      onChange={onChange}/>
-
-//<ACSListController object_type={object_type}  rab_component_model={rab_component_model}/>)
-
-   const [age, setAge] = React.useState('10');
+  rab_component_model.list.components.list = RABSelectObject
   switch (mode) {
     case "text", "view":
       return field_display_value?field_display_value:" "
       break   
     case "edit":
     case "create":
-      return (
-        <Fragment><form>
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            autoFocus={autoFocus}
-            onBlur={onBlur}
-            onChange={onChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl></form></Fragment>)
-
+      return (<ACSListController object_type={object_type} field_list={["alpha_2", "name"]} rab_component_model={rab_component_model} list_select_form_name={formValues_name} list_onSubmit={onSubmit} list_field_value={field_value} list_onChange={onChange} list_select_key_field={select_key_field} list_select_display_field={select_display_field} list_autoFocus={autoFocus} />)
       break
     case "csv":
       return '"'+field_display_value+'""'
