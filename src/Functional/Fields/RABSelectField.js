73999a6ef12deb849740bb801a04f303cc23d1c2
teadams <tracyeadams@gmail.com>
2020-05-17 00:38:53 -0400

    select list with tree data and padding

--- src/Functional/Fields/RABSelectField.js
+++ src/Functional/Fields/RABSelectField.js
@@ -18,6 +18,45 @@ import rab_component_models from '../../Models/HealthMe/component.js'
 //   helperText={field.helper_text}
 //   value=  {this.getDisplayView(object_type,field,prefix)}
 //  style={{width:"90%"}}
+
+// Make the loop a Function
+// Call from top  
+// Put in the child
+// Get correct query
+// add levels
+function formTreeData(data, tree_depth=0) {
+  let tree_data = []
+  data.map(row => {
+      row.tree_depth = tree_depth
+      tree_data.push(row)
+      if (data.children && data.children.length >0) {
+          tree_depth +=1
+          tree_data = tree_data.concat(formTreeData(data.children, tree_depth))
+      }
+  })
+  return tree_data
+}
+
+function padding(num) {
+  let i;  
+  let padding = ""
+  for (i = 0; i < num; i++) {
+    padding = padding + "&npsp;&nbsp;"
+  }
+  return padding
+}
+
+function selectItems(data, select_key_field, select_display_field) {
+    data = formTreeData(data)
+    return (
+      data.map (row => {
+        return(
+          <MenuItem value={row[select_key_field]}>{padding(row.tree_depth)}{row[select_display_field]}</MenuItem>
+          )
+        })
+    )
+}
+
 function RABSelectObject(props) {
   return (<FormControl>
     <Select
@@ -27,16 +66,17 @@ function RABSelectObject(props) {
       value={props.value}
       autoFocus={props.autoFocus}
       onBlur={props.onSubmit}
-      onChange={props.onChange}
-    >{props.data.map (row => {
-      return (<MenuItem value={row[props.select_key_field]}>{row[props.select_display_field]}</MenuItem>)
-      })}
+      onChange={props.onChange}>
+      {props.data && selectItems(props.data,props.select_key_field,props.select_display_field)}
     </Select>
   </FormControl>)
 }
 
 function RABSelectField(props) {
   const {mode, data, field_name, formdata, formValues, onSubmit, onFieldBlur,  onChange, autoFocus, object_type, field_model} = props
+  //data and field name specific the selected value for View mode
+  // field_model specificy the select and dispaly fileds
+  // formValues holds the controlled data
   const formValues_name = field_model.formValues_name
   const field_value = formValues[formValues_name]
   const field_display_value = data[field_name]
@@ -50,7 +90,7 @@ function RABSelectField(props) {
       break   
     case "edit":
     case "create":
-      return (<ACSListController object_type={object_type} field_list={["alpha_2", "name"]} rab_component_model={rab_component_model} list_select_form_name={formValues_name} list_onSubmit={onSubmit} list_field_value={field_value} list_onChange={onChange} list_select_key_field={select_key_field} list_select_display_field={select_display_field} list_autoFocus={autoFocus} />)
+      return (<ACSListController object_type={object_type}  rab_component_model={rab_component_model} list_select_form_name={formValues_name} list_onSubmit={onSubmit} list_field_value={field_value} list_onChange={onChange} list_select_key_field={select_key_field} list_select_display_field={select_display_field} list_autoFocus={autoFocus} />)
       break
     case "csv":
       return '"'+field_display_value+'""'
