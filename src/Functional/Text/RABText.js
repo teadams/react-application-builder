6241e85a97aabf23f65093a18eff50d1fd709922
teadams <tracyeadams@gmail.com>
2020-05-19 11:22:02 -0400

    cleanup

---
+++ src/Functional/Text/RABText.js
@@ -0,0 +1,21 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import { makeStyles } from '@material-ui/core/styles';
+import * as u from '../../Utils/utils.js';
+import { withStyles } from '@material-ui/core/styles';
+import React, { Component, Fragment,  useState, useEffect} from 'react';
+import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField, Select, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar, TableCell,InputLabel } from '@material-ui/core';
+import ACSListController from '../ACSListController.js'
+import rab_component_models from '../../Models/HealthMe/component.js'
+import * as meta from '../../Utils/meta.js';
+
+function RABText(props) {
+  const {header=false, variant=header?"h5":"body1", text} = props
+  let {style} = props
+  if (!header) {
+    style = Object.assign({display:"inline"}, style)
+  }
+  return (<Typography variant={variant} style={style}>{text}</Typography>)
+}
+
+export default RABText
