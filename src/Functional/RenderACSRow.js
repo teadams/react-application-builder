d3c478d6fcd0689a9b8b17ef0cd0a64fdca49359
teadams <tracyeadams@gmail.com>
2020-05-19 23:49:28 -0400

    nice wording

--- src/Functional/RenderACSRow.js
+++ src/Functional/RenderACSRow.js
@@ -10,24 +10,29 @@ import ACSField from './ACSField2.js'
 import ACSListController from './ACSListController.js'
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
 import useGetObjectList from '../Hooks/useGetObjectList';
+import useGetModel from '../Hooks/useGetModel';
 import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField
 , Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar } from '@material-ui/core';
 
 function RABFormWrap(props) {
+  const {object_type} = props
+  const object_types = useGetModel("object_types")
+  if (!object_types) {return null}
   function handleOnClose() {
     if (props.onClose) {
         props.onClose()
     }
   }
-
-  // trying experiment. Dialog can close either
-  // via props open or internal.  
-  if (props.form && (props.mode === "edit" || props.mode === "create")) {    
+  if (props.form && (props.mode === "edit" || props.mode === "create")) {
+    const object_type_model = object_types[object_type]
+    const object_type_pretty_name = object_type_model.pretty_name
+    const form_message = (props.mode==="create")?object_type_model.create_message:object_type_model.edit_message
     return (
       <Dialog fullWidth={true} open={props.open} onClose={handleOnClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">{props.mode} OBJECT TYPE</DialogTitle>
+      <DialogTitle id="form-dialog-title">{u.capitalize(props.mode)} {u.capitalize(object_type_pretty_name)}</DialogTitle>
         <DialogContent>
-          <DialogContentText>FORM MESSAGE</DialogContentText>
+          {form_message && 
+          <DialogContentText>{form_message}</DialogContentText>}
           <form onSubmit={props.onSubmit}>
           {props.children}
           <DialogActions>
@@ -48,7 +53,7 @@ function RABFormWrap(props) {
 
 function RenderACSRow(props) {
   const { ...params} = props
-  const {key_id, form, mode, form_open, data, rab_component_model, field_list, handleFormChange, handleFormSubmit, formValues, onClose} = props
+  const {key_id, form, mode, form_open, data, rab_component_model, object_type, field_list, handleFormChange, handleFormSubmit, formValues, onClose} = props
 
   const FormWrap = RABFormWrap
 
@@ -61,7 +66,7 @@ function RenderACSRow(props) {
           </HeaderWrap>
           {field_list.map((section_fields, s_index) => {
             return (
-              <FormWrap mode={mode} form={form} open={form_open} onSubmit={handleFormSubmit} onClose={onClose} key={s_index+"form"}>
+              <FormWrap mode={mode} form={form} object_type = {object_type} open={form_open} onSubmit={handleFormSubmit} onClose={onClose} key={s_index+"form"}>
               <SectionWrap {...params} key={s_index+"wrap"}>
                 <SectionHeader {...params} key={s_index+"header"}/>
                 {section_fields.map((field_chunk, f_index) => {
