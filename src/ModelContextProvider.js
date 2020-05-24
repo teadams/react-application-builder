00fefe4533bc87e11703ace902c0dc2c3b477734
teadams <tracyeadams@gmail.com>
2020-05-24 19:52:27 -0400

    loading the model_results

--- src/ModelContextProvider.js
+++ src/ModelContextProvider.js
@@ -1,19 +1,30 @@
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
 import ModelContext from "./ModelContext.js"
-
+import * as u from "./Utils/utils.js"
+import * as meta from "./Utils/meta.js"
 
 function ModelContextProvider(props) {
   const [app_params, setAppParams] = useState("");
 
-  return (
-    <ModelContext.Provider
-      value={{
-      app_params: app_params,
-      setAppParams:  (app_params)=> {
-      setAppParams(app_params)    
-      }}}>
+  if (!app_params) {
+    meta.load("app_params", model_results => {
+          setAppParams(model_results)
+    })
+  }
+
+  if (!app_params) {
+    return null
+  } else {
+    return (
+      <ModelContext.Provider
+        value={{
+        app_params: app_params,
+        setAppParams:  (app_params)=> {
+            setAppParams(app_params)    
+        }}}>
         {props.children}
-    </ModelContext.Provider>)
+      </ModelContext.Provider>)
+  }
 }
 
 export default ModelContextProvider
