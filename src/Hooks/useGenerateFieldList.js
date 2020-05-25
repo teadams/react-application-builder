b815f5445fe65421e642b77815fcbf2b42106bc5
teadams <tracyeadams@gmail.com>
2020-05-24 21:32:17 -0400

    convert to model in context

--- src/Hooks/useGenerateFieldList.js
+++ src/Hooks/useGenerateFieldList.js
@@ -5,11 +5,11 @@ import * as meta from '../Utils/meta.js';
 import useGetModel from '../Hooks/useGetModel';
 
 const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,  field_list=[]) => {
-  const field_models =  useGetModel("fields")
-  if (!field_models) return []
+  const object_model =  useGetModel("object_types", object_type)
+  const field_models =  useGetModel("fields", object_type)
   // form not needed or inputs not ready
 
-  const id_field = meta.keys(object_type).key_id
+  const id_field = object_model.key_id
 
   if (field_list && field_list.length >0 && !object_type) {
       return field_list
@@ -19,7 +19,7 @@ const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,
     field_list = [id_field, field_name]
   } else {
     if (object_type) {
-      field_list = Object.keys(field_models[object_type])
+      field_list = Object.keys(field_models)
     } else {
       if (data) {
         field_list = Object.keys(data[0])
@@ -35,10 +35,10 @@ const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,
  
   let scrubbed_field_list = []
   field_list.forEach(field => {
-    const f_model = field_models[object_type][field]
-    if (!f_model.prevent_view 
-            && !(form && f_model.not_on_row_form)
-            && !(form && mode==="create" && f_model.not_on_create_form)) {
+    const field_model = field_models[field]
+    if (!field_model.prevent_view 
+            && !(form && field_model.not_on_row_form)
+            && !(form && mode==="create" && field_model.not_on_create_form)) {
           scrubbed_field_list.push(field)
       }
   })
