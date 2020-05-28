cdc31d9e579f00b35802e6722ddf9df3c9deff90
teadams <tracyeadams@gmail.com>
2020-05-27 23:02:57 -0400

    pagination!

--- src/Functional/RenderACSList.js
+++ src/Functional/RenderACSList.js
@@ -10,12 +10,26 @@ import React, { Component, Fragment,  useState, useContext, useEffect} from 'rea
 import { FormControl, FormLabel, FormGroup, FormControlLabel, Chseckbox, Typography, Chip, Grid, MenuItem, TextField, TableContainer, TableHead, TableCell, TableRow, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, TablePagination, Button, Paper, Avatar, TableBody, Table } from '@material-ui/core';
 
 function RenderACSList(props) {
-  const {data,rab_component_model} = props
+  const {data, rab_component_model} = props
   const {...params} = props
   const {header_wrap:HeaderWrap, header:Header, list_wrap:ListWrap, list_header_wrap:ListHeaderWrap, list_header:ListHeader, body_wrap:BodyWrap, list:RABList, footer_wrap:FooterWrap, footer:Footer,
   list_container:ListContainer, list_pagination:ListPagination} = rab_component_model.list.components 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    if (data) {
+  const handleChangePage = (event, newPage) => {
+     setPage(newPage);
+   };
+ 
+  const handleChangeRowsPerPage = (event) => {
+     setRowsPerPage(parseInt(event.target.value, 10));
+     setPage(0);
+  };
+
+    if (data && data.length>0) {
+      const count = data.length
+      const paginated_data =  data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) 
+      params.data=paginated_data
       return ( 
         <Fragment>
           <HeaderWrap {...params}>
@@ -32,13 +46,13 @@ function RenderACSList(props) {
              </ListWrap>
           </ListContainer>
           <ListPagination
-             rowsPerPageOptions={[5, 10, 25]}
+             rowsPerPageOptions={[10, 20, 30]}
              component="div"
-             count={20}
-             rowsPerPage={10}
-             page={1}
-             onChangePage=""
-             onChangeRowsPerPage=""
+             count={count}
+             rowsPerPage={rowsPerPage}
+             page={page}
+             onChangePage={handleChangePage}
+             onChangeRowsPerPage={handleChangeRowsPerPage}
            />
         </Fragment>
     )
