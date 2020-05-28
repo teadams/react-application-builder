570faacbadd6fdc9e30d958682dae761c01d2120
teadams <tracyeadams@gmail.com>
2020-05-28 10:34:31 -0400

    prevent pagination

--- src/RABComponents/TreeMenu.js
+++ src/RABComponents/TreeMenu.js
@@ -43,7 +43,9 @@ function TreeMenu(props)  {
   } 
 
   const rab_component_model = {
-          list:{names:{header_wrap:"Fragment",list_container:"Fragment", header:"RABVoid",list_header_wrap:"RABVoid", body_wrap:"Fragment"}},
+          list:{names:{header_wrap:"Fragment",list_container:"Fragment", header:"RABVoid",list_header_wrap:"RABVoid", body_wrap:"Fragment", list_pagination:"Fragment"},
+          props:{pagination:false}
+          },
           row:{components:{row_wrap:RenderTreeItem},
               names:{header:"RABVoid"}},
           field:{component_names:{field_wrap:"Fragment", field:"Fragment"}},
