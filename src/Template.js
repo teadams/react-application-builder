93797bfa30604033bff7688189ee7f8b269bc398
teadams <tracyeadams@gmail.com>
2020-05-28 20:41:09 -0400

    pretty up context select

--- src/Template.js
+++ src/Template.js
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { fade, withStyles, makeStyles } from '@material-ui/core/styles';
 import React, { Component, Fragment} from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import Badge from '@material-ui/core/Badge';
@@ -37,6 +37,30 @@ function Template(props) {
         display: 'block',
       },
     },
+    context: {
+       position: 'relative',
+       borderRadius: theme.shape.borderRadius,
+       marginRight: theme.spacing(2),
+       marginLeft: 0,
+       width: '100%',
+       [theme.breakpoints.up('sm')]: {
+         marginLeft: theme.spacing(3),
+         width: 'auto',
+       },
+     },
+     inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+          width: '20ch',
+        },
+      },
     sectionDesktop: {
        display: 'none',
        [theme.breakpoints.up('md')]: {
@@ -59,7 +83,9 @@ function Template(props) {
       <Toolbar className={classes.root}>
           <DrawerMenu menu_type="hamburger" selected_menu={selected_menu} />
           <Typography className={classes.title} variant="h6" noWrap>{app_params.name}</Typography>
+          <div className={classes.context}>
           <ContextSelect/>
+          </div>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
               <IconButton aria-label="show 4 new mails" color="inherit">
