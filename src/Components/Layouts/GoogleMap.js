de21b4b36e5597166523c32dcdd414132843a0e3
teadams <tracyeadams@gmail.com>
2020-06-02 11:34:19 -0400

    layout

--- src/Components/Layouts/GoogleMap.js
+++ src/Components/Layouts/GoogleMap.js
@@ -146,7 +146,7 @@ function GoogleMap (props) {
           </Grid>
           <Grid item  style={{padding:20}}> <Button variant="contained" onClick={handleCreateProjectOpen}>Create a Project</Button></Grid>
           <Grid item  style={{padding:20}}>
-            <ACSObjectCount api_options={{get_count:true, num_rows:1,}} text="Number of active projects:" object_type="nwn_project"/> 
+            <ACSObjectCount api_options={{get_count:true, num_rows:1,}} text="Number of active projects:" object_type="nwn_project"/>
 
             <ACSObjectCount api_options={{get_count:true, num_rows:1, filter_id:"Success", filter_field:"status"}} text="Number of sucessful projects:" object_type="nwn_project"/>
 
