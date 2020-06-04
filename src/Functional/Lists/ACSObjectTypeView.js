42e1d8ab3bf12e4be68fcfe48ef169e60969f04d
teadams <tracyeadams@gmail.com>
2020-06-04 16:47:46 -0400

    getting rid of unnecessary imports

--- src/Functional/Lists/ACSObjectTypeView.js
+++ src/Functional/Lists/ACSObjectTypeView.js
@@ -1,20 +1,8 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
-import { makeStyles } from '@material-ui/core/styles';
-import * as log from '../Utils/log.js'
-import * as meta from '../Utils/meta.js'
-import * as data from '../Utils/data.js';
-import * as u from '../Utils/utils.js';
-import FieldView from './FieldView.js'
-import {functional_components} from "../Functional/index.js"
-import ACSField from '../Functional/ACSField2.js'
-import RenderACSField from '../Functional/RenderACSField.js'
-import ACSRowController from '../Functional/ACSRowController.js'
-import ACSListController from '../Functional/ACSListController.js'
+import * as u from '../../Utils/log.js'
+import ACSListController from '../ACSListController.js'
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
-import {Tab, Tabs, Menu, MenuItem, Paper, MenuList,List,ListItem,ListItemAvatar,ListItemIcon,ListItemSecondaryAction,ListItemText,ListSubheader,Table,TableBody,TableCell,TableContainer,TableFooter,TableHead,TablePagination,TableRow,Typography} from '@material-ui/core';
-import useGetModel from "../Hooks/useGetModel.js"
-import AuthContext from '../Components/User/AuthContext';
 
 
 function ACSObjectTypeView(props)  {
