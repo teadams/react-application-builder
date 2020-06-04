5aca6d78b6ecfac2839ecc339e0acdd656cc79fd
teadams <tracyeadams@gmail.com>
2020-06-04 13:48:13 -0400

    initial version of NWAProjectView

--- src/Functional/Rows/ACSObjectView.js
+++ src/Functional/Rows/ACSObjectView.js
@@ -5,15 +5,16 @@ import ACSRowController from '../ACSRowController.js'
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
 
  function ACSObjectView(props)  {
-  const {object_type, field_list, api_options, num_columns, form_open=props.row_form, ...params} = props
-  // XX convert field_field and filter_id to api_options
+  const {object_type, id, api_options, field_list, num_columns, form_open=props.row_form, ...params} = props
+  // XX now-look up by id.
+  // later may allow other unique columns
   function handleFormClose() {
     if (props.onClose) {
       props.onClose()
     }
   }
   return ( 
-      <ACSRowController {...params} form_open={form_open} onClose={handleFormClose} object_type={object_type} field_list={field_list} api_options={api_options} num_columns={num_columns}  />
+      <ACSRowController {...params} form_open={form_open} onClose={handleFormClose} object_type={object_type} id={id} field_list={field_list} api_options={api_options} num_columns={num_columns}  />
   )
 }
 export default ACSObjectView;
