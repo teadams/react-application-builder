5d125d57cb1e24bae7b05284aa9e6e526a605f48
teadams <tracyeadams@gmail.com>
2020-06-10 21:23:33 -0400

    combining text, url and image

--- src/Functional/Fields/RABTextField.js
+++ src/Functional/Fields/RABTextField.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import * as u from '../../Utils/utils.js';
 import { withStyles } from '@material-ui/core/styles';
 import React, { Component, Fragment,  useState, useEffect} from 'react';
-import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField, Select, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar, TableCell,InputLabel } from '@material-ui/core';
+import {Link, FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField, Select, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar, TableCell,InputLabel } from '@material-ui/core';
 import ACSImage from './ACSImage.js'
 import ACSListController from '../ACSListController.js'
 import rab_component_models from '../../Models/HealthMe/component.js'
@@ -13,14 +13,14 @@ import * as meta from '../../Utils/meta.js';
 
 function RABTextField(props) {
   const {mode, data, field_name, field_model={}, formdata, formValues, disable_underline=false, onChange, autoFocus, fullWidth=true, image_size="small"} = props
-  let {with_thumbnail=""} = props
+  let {with_thumbnail="", with_url=""} = props
   // XX field model passed due to referenced change. May 
   // be done server side later
   let field_value=""
   field_value = data[field_model.final_field_name?field_model.final_field_name:field_name]
 
   with_thumbnail = with_thumbnail?with_thumbnail:field_model.with_thumbnail
-                        
+  with_url = with_url?with_url:field_model.with_url
   switch (mode) {
     case "edit":
     case "create":
@@ -44,16 +44,26 @@ function RABTextField(props) {
     case "text":
       return field_value?field_value:" "
     default:
-      if (!with_thumbnail) {    
+      if (!with_thumbnail && !with_url || (with_url && !data[with_url])) {    
         return field_value?field_value:" "
+      } else if (!with_thumbnail && with_url) {
+        return (<Link href={data[with_url]}>{field_value}</Link>)
       } else {
           const thumbnail = data[with_thumbnail]
-          if (thumbnail) {  
-            return (<div style={{display:"flex"}}> <ACSImage image_object={thumbnail} fix="width" size={image_size}/>&nbsp; {field_value}</div>)
+          let url = with_url?data[with_url]:""
+          if (thumbnail) { 
+            if (url) {
+              return (<div style={{display:"flex"}}> <ACSImage image_object={thumbnail} fix="width" size={image_size}/>&nbsp;<Link href={url}>{field_value}</Link></div>)
+            } else {
+              return (<div style={{display:"flex"}}> <ACSImage image_object={thumbnail} fix="width" size={image_size}/>&nbsp; {field_value}</div>)
+            }
           } else {
             const letters = field_value?field_value.charAt(0):""
-            return (<div style={{display:"flex"}}><ACSImage letters={letters}  fix="width" size={image_size}/>&nbsp;
-          {field_value}</div>)
+            if (url) {
+              return (<div style={{display:"flex"}}><ACSImage letters={letters}  fix="width" size={image_size}/>&nbsp;<Link href={url}>{field_value}</Link></div>)    
+            } else {
+              return (<div style={{display:"flex"}}><ACSImage letters={letters}  fix="width" size={image_size}/>&nbsp;{field_value}</div>)
+            }
           }
       }
   }
