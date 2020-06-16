bed982c7bfe14b4e4c31417346760733e0d25c5b
teadams <tracyeadams@gmail.com>
2020-06-16 14:05:14 -0400

    passing layout and section_list to ACSRoWController

--- src/Functional/Rows/ACSObjectView.js
+++ src/Functional/Rows/ACSObjectView.js
@@ -5,7 +5,7 @@ import ACSRowController from '../ACSRowController.js'
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
 
  function ACSObjectView(props)  {
-  const {object_type, id, api_options, field_list, num_columns, form_open=props.row_form, ...params} = props
+  const {object_type, id, api_options, layout, sections, field_list, num_columns, form_open=props.row_form, ...params} = props
   // XX now-look up by id.
   // later may allow other unique columns
   function handleFormClose() {
@@ -14,7 +14,7 @@ import React, { Component, Fragment,  useState, useContext, useEffect} from 'rea
     }
   }
   return ( 
-      <ACSRowController {...params} form_open={form_open} onClose={handleFormClose} object_type={object_type} id={id} field_list={field_list} api_options={api_options} num_columns={num_columns}  />
+      <ACSRowController {...params} form_open={form_open} onClose={handleFormClose} object_type={object_type} id={id} layout={layout} secitons={sections} field_list={field_list} api_options={api_options} num_columns={num_columns}  />
   )
 }
 export default ACSObjectView;
