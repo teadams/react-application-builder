a867435441f2d6cfd5153a9680fc43e5307eef9f
teadams <tracyeadams@gmail.com>
2020-06-18 21:21:49 -0400

    role approve... with refresh of the data state

--- src/MessageIcon.js
+++ src/MessageIcon.js
@@ -13,6 +13,7 @@ import {CrudTable, Text, GoogleMap} from './Components/Layouts';
 import {NavMenuLink} from './Components/Experimental';
 import ACSObjectCount from './Functional/Text/ACSObjectCount.js'
 import ACSObjectTypeView from './Functional/Lists/ACSObjectTypeView.js'
+import ACSObjectView from './Functional/Rows/ACSObjectView.js'
 import ACSField from './Functional/ACSField2.js'
 import {ContextSelect, AuthToggleLink, AuthContext, AuthContextProvider, Auth} from './Components/User';
 import {SelectObject} from './Components/FormsAndViews';
@@ -34,7 +35,8 @@ function MessageIcon(props) {
   const [anchorEl, setAnchorEl] = useState(null);
   const [message_data, setMessageData] = useState(null);
   const [message_open, setMessageOpen] = useState(false);
-    
+  const [role_data, setRoleData] = useState(null)
+
   function handleMessageClick(event, id, type,field_name, data) {
     if (message_open) {
         handleMessageClose()
@@ -51,6 +53,7 @@ function MessageIcon(props) {
             if (error) {
               alert("error is " + error)
             } else {
+              // could also refetch data
               let read_update_message_data = Array.from(message_data)
               let message
               for (message of read_update_message_data) {
@@ -67,6 +70,32 @@ function MessageIcon(props) {
 
   function handleMessageClose() {
     setMessageOpen(false)
+    setRoleData(null)
+  }
+
+  function handleRoleData(role_data) {
+    setRoleData(role_data)
+  }
+
+  function handleRoleApprove() {
+
+    const approve_object = {id:message_open.applicant_subsite_role.id, status:"Approved"}
+    api.postData("core_subsite_role", approve_object, {}, (result, error) => { 
+        if (error) {
+          alert("error is " + error)
+        } else {
+          let approve_update_message_data = Array.from(message_data)
+          let message
+          for (message of approve_update_message_data) {
+              if (message.id === message_open.id) {
+                message.applicant_subsite_role.status = "Approved"
+                continue
+              }
+              setMessageData(approve_update_message_data)
+          }
+        }
+    })
+    handleMessageClose()
   }
 
   function MessageRow(row_props) {
@@ -78,19 +107,28 @@ function MessageIcon(props) {
           const emphasis = data.read_p?"":"bold"
           return <ACSField onFieldClick={handleMessageClick} field_mode={mode} field_form={false} field_name={field_name} emphasis={emphasis} {...row_params} key={field_name} key_id={field_name}/>
         })}
-        {message_open && 
+        {message_open && (!message_open.applicant_subsite_role || message_open.applicant_subsite_role.status !== "Applied" || role_data) &&
         <Dialog fullWidth={true} open={message_open} onClose={handleMessageClose} aria-labelledby="form-dialog-title">
           <DialogTitle id="form-dialog-title">{message_open.subject}</DialogTitle>
             <DialogContent>
               <div style={{display:'flex'}}>
-                <div>From:&nbsp;</div><div><ACSField data={message_open} object_type="core_message" field_mode="view" field_model={{with_thumbnail:false}} field_form={false} with_thumbnail={false} field_name="from_user" key="from_user" key_id="from_user"/></div>
+                <div><Typography>From:&nbsp;</Typography></div><div><Typography><ACSField data={message_open} object_type="core_message" field_mode="view" field_model={{with_thumbnail:false}} field_form={false} with_thumbnail={false} field_name="from_user" key="from_user" key_id="from_user"/></Typography></div>
                 <div style={{flexGrow:1}}/>
-                <div>Date: <ACSField data={message_open} object_type="core_message" field_mode="view" field_form={false} field_name="last_updated_date" key="last_updated_date" key_id="last_updated_date"/></div>
+                <div><Typography>Date: <ACSField data={message_open} object_type="core_message" field_mode="view" field_form={false} field_name="last_updated_date" key="last_updated_date" key_id="last_updated_date"/>  </Typography></div>
+              
               </div>
-              <div style={{paddingBottom:20}}/>
-              <ACSField data={message_open} object_type="core_message" field_mode="view" field_form={false} field_name="body" key="body" key_id="body"/>
+              <div style={{paddingBottom:20}}/><Typography style={{paddingBottom:20}}>
+              <ACSField data={message_open} object_type="core_message" field_mode="view" field_form={false} field_name="body" key="body" key_id="body"/></Typography>
+              {role_data &&
+                  <Typography><i>This message is an application for the role <b>role_data.name}</b> in <b>{message_open.core_subsite.name}</b>.  To accept, click "Approve". Otherwise, click "Close".</i></Typography>
+              }
             </DialogContent>
           <DialogActions>
+          {role_data &&
+          <Button onClick={handleRoleApprove} color="primary">
+            Approve
+          </Button>
+          }
           <Button onClick={handleMessageClose} color="primary">
             Close
           </Button>
@@ -99,6 +137,12 @@ function MessageIcon(props) {
       </Fragment>)
 
   }
+
+// if applicant_subsite_role -- 
+// and status is Applied
+// core_role
+// leader notes
+// Then ask for approval
 
   function handleMessageCount(count) {
     setMessageCount(count)
@@ -123,6 +167,10 @@ function MessageIcon(props) {
   return    ( 
     <Fragment>
     {user_id && <ACSObjectCount headless={true} object_type="core_message" api_options={{get_count:true, num_rows:1, filter_id:user_id+","+false, filter_join:"and", filter_field:"to_user,read_p"}} onData={handleMessageCount}/> }
+
+    {message_open && message_open.applicant_subsite_role && message_open.applicant_subsite_role.status === "Applied" &&         
+        <ACSObjectView onData={handleRoleData} headless={true} object_type="core_role"  api_options={{filter_id:message_open.applicant_subsite_role.core_role, filter_field:"id"}}/>}
+
     <IconButton aria-label="show new mails" color="inherit" onClick={handleClick}>
         <Badge badgeContent={message_count} color="secondary">
             <MailIcon />
