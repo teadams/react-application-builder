e46ad7230055bb086d90046efb1f0d9c9e81be7a
teadams <tracyeadams@gmail.com>
2020-06-19 16:10:02 -0400

    adding back image

--- src/Functional/Fields/User.js
+++ src/Functional/Fields/User.js
@@ -12,7 +12,9 @@ import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typograp
 import ACSImage from "./ACSImage.js"
 
 function User(props) {
-  const {data, field_name, display, mode="view", with_thumbnail, image_size,  ...params} = props
+  const {data, field_name, display, mode="view", field_model, image_size,  ...params} = props
+  const {with_thumbnail} = field_model
+
   let {size="tiny"} = props
   let thumbnail = ""
   // XX get rid of size. depricate
