5d89cdda049a89583ec1c7eb46338a545b01a861
teadams <tracyeadams@gmail.com>
2020-06-19 23:31:27 -0400

    auth on create button

--- src/Functional/Buttons/ACSCreateButton.js
+++ src/Functional/Buttons/ACSCreateButton.js
@@ -6,6 +6,9 @@ import {IconButton} from '@material-ui/core';
 import IconCreate from "@material-ui/icons/Add";
 import * as u from '../../Utils/utils.js'
 import ACSObjectView from '../Rows/ACSObjectView.js'
+import {Auth} from '../../Components/User/index.js';
+
+
 function ACSCreateButton(props) {
   const {object_type, Component, layout, sections, field_list, dialog_size} = props
   const [create_dialog, setCreateDialog] = useState(false);
@@ -33,6 +36,7 @@ function ACSCreateButton(props) {
   const float=props.float?props.float:'none'
  return (
       <Fragment>
+      <Auth auth_action="create" object_type={object_type} prompt_login={false}>
       {Component?
        <Component text="here" onClick={handleOnClick}/>
       : <IconButton variant="fab" color="primary"  style={{ display:"inline"}} onClick={handleOnClick}>
@@ -41,6 +45,7 @@ function ACSCreateButton(props) {
       }
       {create_dialog  &&
       <ACSObjectView object_type={object_type} row_mode="create" row_form="true" layout={layout} sections={sections} onSubmit={handleOnSubmit} onClose={handleOnClose} field_list={field_list} dialog_size={dialog_size} /> }
+      </Auth>
       </Fragment>
       )
 }
