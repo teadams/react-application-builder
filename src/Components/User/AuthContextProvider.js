f44206493c6edecbb570713de6e590e7521fbef1
teadams <tracyeadams@gmail.com>
2020-07-02 21:42:10 -0400

    setting default context in authContextProvider

--- src/Components/User/AuthContextProvider.js
+++ src/Components/User/AuthContextProvider.js
@@ -1,12 +1,18 @@
 import React, {  useState} from 'react';
 import AuthContext from './AuthContext.js'
-
+import useGetModel from "../../Hooks/useGetModel.js"
 
 function AuthContextProvider(props) {
   const [user, setUser] = useState("");
   const [context_id, setContextId] = useState("");
   const [dirty_stamp, setDirtyData] = useState(Date.now());
 
+  const app_params =  useGetModel("app_params")
+  const default_context =app_params["context_default_object"]
+  if (!context_id && default_context) {
+    setContextId(default_context)
+    return null
+  }
   return (
     <AuthContext.Provider
       value={{
