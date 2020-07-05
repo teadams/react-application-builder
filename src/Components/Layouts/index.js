3c93ea3bbee73a817471bfc2943f01b72ca0cb6e
teadams <tracyeadams@gmail.com>
2020-07-05 14:18:04 -0400

    removing unecessary

--- src/Components/Layouts/index.js
+++ src/Components/Layouts/index.js
@@ -3,12 +3,11 @@ import SelectField from './SelectField';
 import DeleteButton from './DeleteButton';
 import EditButton from './EditButton';
 import GoogleMap from './GoogleMap';
-import Cell from './Cell';
 import SelectCell from './SelectCell';
 import MenuLink from './MenuLink';
 import Text from './Text';
 
 
 export {
-    MenuLink,SelectCell, MenuBar, SelectField, Cell, Text, GoogleMap
+    MenuLink,SelectCell, MenuBar, SelectField, Text, GoogleMap
 }
