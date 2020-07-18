c308a6b792c90e3e67d149e436f57f1dfc8d707f
teadams <tracyeadams@gmail.com>
2020-07-17 20:28:23 -0400

    removing excess iports

--- src/Functional/Lists/ACSMap.js
+++ src/Functional/Lists/ACSMap.js
@@ -1,21 +1,16 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 
-import React, {useState, Fragment} from 'react';
-import {Paper,  Typography, Button, Grid, Popover} from '@material-ui/core';
-//import * as meta from '../../Utils/meta.js'
-
-import ACSCreateButton from '../../Functional/Buttons/ACSCreateButton.js'
-import ObjectView from '../../RABComponents/ObjectView.js'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import React, {useState, useContext, Fragment} from 'react';
+import {Typography} from '@material-ui/core';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import * as log from '../../Utils/log.js'
 import * as meta from '../../Utils/meta.js';
 import * as api from '../../Utils/data.js';
 import * as u from '../../Utils/utils.js'
 import * as google_map from './api.js'
-import {  BrowserRouter as Router,  Switch,  Route,  Link,  Redirect, useHistory } from "react-router-dom";
-//import GoogleAPIWrapper from "./GoogleAPIWrapper.js"
+import {AuthContext} from '../../Components/User';
+
 
 function get_image_url (image_object) {
     const image_base = (process.env.NODE_ENV ==="production")? "https://storage.googleapis.com/acs_full_stack/":""
@@ -28,22 +23,48 @@ function get_image_url (image_object) {
 }
 
 function ACSMap (props) {
-  const {object_type,  data, onClick, latitude_field="latitude", longitude_field="longitude", initial_zoom=7, onMarkerClick, onMapClick, onMouseover, PopupComponent} = props
-  const [map_data, setMapData] = useState(data)
+  const {object_type, onClick, latitude, longitude, latitude_field="latitude", longitude_field="longitude", initial_zoom=3, onMarkerClick, onMapClick, onMouseover, PopupComponent, centerAroundCurrentLocation=false, centerAroundSubsiteLocation=true} = props
+  const [map_data, setMapData] = useState(props.map_data)
+  const [subsite_data, setSubsiteData] = useState(props.subsite_data)
+
   const [showInfoWindow, setShowInfoWindow] =useState(false)
   const [activeMarker, setActiveMarker] = useState({})
-
   const [selectedPlace, setSelectedPlace]= useState({marker_data:{}})
 
   const [anchor, setAnchor] = useState(null);
   //const [center, setCenter] = useState({lat:"26.599674",	lng:"-81.853839"});
-  const [center, setCenter] = useState({lat:70.599674, lng:-81.853839});
+  const [prior_props_center, setPriorPropsCenter] = useState({lat:latitude, lng:longitude});
+  const [center, setCenter] = useState({});
 
-// TODO - default lat and long
-      // from props
-      // from subsi
-      // from user's organization
-// TODO - move map
+  const context = useContext(AuthContext)
+
+  if (!map_data) {
+    api.getData(object_type, "", (map_data, error) => {
+      setMapData(map_data)
+    })
+  }
+
+  // take initial center from subsite
+  if (map_data && !subsite_data && context.context_id && centerAroundSubsiteLocation && !centerAroundCurrentLocation) {
+    api.getData("core_subsite", {id:context.context_id}, (api_subsite_data, error) => {
+      setSubsiteData(api_subsite_data[0])
+      if  (api_subsite_data[0].latitude && api_subsite_data[0].longitude)  {
+        setCenter({lat:api_subsite_data[0].latitude,	lng:api_subsite_data[0].longitude});
+      }
+    })
+  }
+
+  if (!map_data) {
+    return null
+  }
+
+  // Handles latitude and longitude changing from props 
+  if  (latitude !== prior_props_center.latitude || longitude !== prior_props_center.longitude)  {
+          setCenter({lat:latitude,	lng:longitude})
+          setPriorPropsCenter({lat:latitude,	lng:longitude})
+  }
+
+  
 // TODO - play with drawing on map
 // TODO - pop up component 
 // TODO - show_popup_thumbnail, show_popup_summary, show_popup_description
@@ -71,20 +92,10 @@ function ACSMap (props) {
     }
   };
 
-  if (!map_data) {
-    api.getData(object_type, "", (map_data, error) => {
-      setMapData(map_data)
-      setCenter({lat:20.599674,	lng:-81.853839});
-    })
-  }
-    
-  if (!map_data) {
-    return null
-  }
 
   return (
       <Fragment> 
-      <Map   const containerStyle = {{position: 'absolute',  width: '75%',height: '75%'}} style = {{position: 'absolute',  width: '100%', height: '100%'}} google={props.google}  onClick={handleMapClick} zoom={initial_zoom} center={center}  centerAroundCurrentLocation={false}>
+      <Map   const containerStyle = {{position: 'absolute',  width: '75%',height: '75%'}} style = {{position: 'absolute',  width: '100%', height: '100%'}} google={props.google}  onClick={handleMapClick} zoom={initial_zoom} center={center}  centerAroundCurrentLocation={centerAroundCurrentLocation}>
             {map_data.map(marker => {
               var icon
               if (marker.type && marker.type.thumbnail) {
