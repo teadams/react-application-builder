cc2302ff862fdd9bfda736599e52fb38cbe57ac7
teadams <tracyeadams@gmail.com>
2020-07-28 20:58:26 -0400

    not bashing files

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -155,13 +155,10 @@ const useForm = (object_type, field_name="", data, handleSubmit, mode="view", fo
       // only send file fields when changed
       Object.keys(formValues).forEach(form_field_name => {
       //  u.a(form_field_name, Object.keys(field_models))
-        const field_model = field_models[object_type][form_field_name]
+        const [base_field_name, final_field_name, base_object_type, final_object_type, base_field_model, final_field_model] = meta.resolveFieldModel(object_type, form_field_name, object_models, field_models)
+
         // user_id, core_subsite might not have field_model
-        if (field_model) {
-          u.a(form_field_name, filesTouched, field_model.input_type)
-        }
-        if (field_model && field_model.input_type === "file" && !filesTouched.includes(form_field_name)) {
-            u.a("delete files that were not touched")
+        if (final_field_model && final_field_model.input_type === "file" && !filesTouched.includes(form_field_name)) {
             delete formValues[form_field_name]
         }
       })
