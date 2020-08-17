b591f06aaa90ad7c29185cb69c91da27143896ca
teadams <tracyeadams@gmail.com>
2020-08-17 00:17:52 -0400

    delayed authorization

--- src/ACSLibrary/Layouts/ACSWizard.js
+++ src/ACSLibrary/Layouts/ACSWizard.js
@@ -2,9 +2,13 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import * as u from '../../Utils/utils.js';
 import * as control from '../../Utils/control.js';
-
 import {ACSRowController} from '../../ACSRenderEngine/index.js'
 import React, { Component, Fragment,  useState, useContext, useEffect} from 'react';
+import {AuthContext, Auth, LoginForm} from '../../Modules/User/index.js';
+
+import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField
+, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar } from '@material-ui/core';
+
 import UIContext from '../../Template/UIContext';
 import useGetModel from '../../Hooks/useGetModel'
 
@@ -33,6 +37,15 @@ import useGetModel from '../../Hooks/useGetModel'
   const {...wizard_props}= wizard_model[current_step].props;
   const WizardComponent = control.componentByName(component_name);
   
-  return (<WizardComponent row_form={true} onClose={handleFormClose} {...wizard_props}/>)
+  //<Dialog fullWidth={true} maxWidth={dialog_size} open={Boolean(props.open)} onClose={handleOnClose} aria-labelledby="form-dialog-title">
+  //<DialogTitle id="form-dialog-title">{form_title?form_title:(u.capitalize(props.mode) + u.capitalize(object_type_pretty_name))}</DialogTitle>
+//    <Auth auth_action="create" object_type="core_subsite" onClose={onClose} data={data}>
+
+  return (
+      <Dialog open={true} fullWidth={true} maxWidth="xl">
+        <DialogTitle> THISI S TEH TITLE</DialogTitle>
+        <WizardComponent row_delayed_auth={true}  row_form={true} no_header={true} row_dialog_center={true} onClose={handleFormClose} {...wizard_props}/>
+      </Dialog>
+      )
 }
 export default ACSWizard;
--- src/Modules/User/index.js
+++ src/Modules/User/index.js
@@ -3,8 +3,10 @@ import AuthContext from './AuthContext';
 import AuthContextProvider from './AuthContextProvider';
 import LoginForm from './LoginForm'
 import Auth from './Auth'
+import DelayedAuth from './DelayedAuth'
+
 import ContextSelect from './ContextSelect'
 
 export {
-    AuthToggleLink, AuthContext, AuthContextProvider, LoginForm, ContextSelect, Auth
+    AuthToggleLink, AuthContext, AuthContextProvider, LoginForm, ContextSelect, Auth, DelayedAuth
 }
