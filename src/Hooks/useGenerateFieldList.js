ee569e01d7e17f39e4716c72b4742480fb2cb451
teadams <tracyeadams@gmail.com>
2020-08-24 22:34:31 -0400

    combo fields get their ilst expanded

--- src/Hooks/useGenerateFieldList.js
+++ src/Hooks/useGenerateFieldList.js
@@ -4,7 +4,20 @@ import * as u from '../Utils/utils.js';
 import * as meta from '../Utils/meta.js';
 import useGetModel from '../Hooks/useGetModel';
 
+const include_field = (field_model, mode, form) => {
+  if (!field_model.prevent_view && !(mode==="view" && field_model.not_on_view) &&
+          !(field_model.not_on_list && mode==="list")
+          && !(form && field_model.not_on_row_form)
+          && !(form && mode==="create" && field_model.not_on_create_form)
+          && !(form && mode==="edit" && field_model.not_on_edit_form)) {
+                return true
+    } else {
+      return false
+    }
+}
+
 const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,  field_list=[], lazy="core", layout, section, field_models, object_models) => {
+
   const default_object_models = useGetModel("object_types")
   const default_field_models =  useGetModel("fields")
   if (!field_models) {
@@ -19,20 +32,29 @@ const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,
   }
   const id_field = object_model.key_id
 
+
   if ((field_list && field_list.length >0)) {
+
       return field_list
   }
 
   if (field_name) {
-    field_list = [id_field, field_name]
+    const field_model = field_models[object_type][field_name]
+    if (field_model.combo_fields) {
+        field_list = [id_field]
+        field_model.combo_fields.forEach(combo_field=>{
+            field_list.push(combo_field)
+        })
+    } else {
+      field_list = [id_field, field_name]
+    }
   } else {
     if (object_type) {
       const base_field_list = object_models[object_type].field_tags[lazy]
       field_list = []
       base_field_list.forEach(field => {
          const field_model = field_models[object_type][field] 
-        field_list.push(field)
-        
+        field_list.push(field)        
       })
     } else {
       if (data) {
@@ -48,16 +70,11 @@ const useGenerateFieldList = (object_type, field_name="", data, mode, form=true,
   }
  
   let scrubbed_field_list = []
-
   field_list.forEach(field => {
-    const [base_field_name, final_field_name, base_object_type, final_object_type, base_field_model, final_field_model] = meta.resolveFieldModel(object_type, field, object_models, field_models)
-    if (!final_field_model.prevent_view && !(mode==="view" && final_field_model.not_on_view) &&
-            !(final_field_model.not_on_list && mode==="list")
-            && !(form && final_field_model.not_on_row_form)
-            && !(form && mode==="create" && final_field_model.not_on_create_form)
-            && !(form && mode==="edit" && final_field_model.not_on_edit_form)) {
-          scrubbed_field_list.push(field)
-      }
+    const field_model = field_models[object_type][field]
+    if (include_field(field_model, form, mode))  { 
+      scrubbed_field_list.push(field)
+    }
   })
 
   return scrubbed_field_list
