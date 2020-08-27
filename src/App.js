0c013e18f699e6ee3f88bf329509bf79a3fbf894
teadams <tracyeadams@gmail.com>
2020-08-27 12:18:43 -0400

    adding pickers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import React, {} from 'react';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
 
 import Template from "./Template/Template.js"
 import {AuthContextProvider} from './Modules/User';
@@ -9,13 +11,15 @@ import UIContextProvider from './Template/UIContextProvider.js';
 
 function App(props) {
   return      (
+  <MuiPickersUtilsProvider utils={MomentUtils}>
     <ModelContextProvider>
       <AuthContextProvider>
-       <UIContextProvider>
+        <UIContextProvider>
             <Template {...props}/>
-      </UIContextProvider>
-    </AuthContextProvider>
-  </ModelContextProvider>
+        </UIContextProvider>
+      </AuthContextProvider>
+    </ModelContextProvider>
+  </MuiPickersUtilsProvider>
   )
 }
 
