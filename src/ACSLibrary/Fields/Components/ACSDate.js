4ce60b07846a081149c25e5450284cd63ff31599
teadams <tracyeadams@gmail.com>
2020-08-30 19:16:04 -0400

    clean up of data field

--- src/ACSLibrary/Fields/Components/ACSDate.js
+++ src/ACSLibrary/Fields/Components/ACSDate.js
@@ -14,7 +14,7 @@ import {
 
 function ACSDate(props) {
   const {mode, row_data,  data, object_type, data_field, pretty_name, display_field=props.field_name, references_field, field_name, form_field_name=props.field_name, field_model={}, object_models, formdata, formValues, disable_underline=false, onChange, autoFocus, fullWidth=true, image_size="small", model_valid_values, valid_values, 
-  variant="outlined", required, helperText, placeholder, multiline=false, more_link_cutoff="", more_link_list_cutoff=props.more_link_cutoff, prevent_edit, input_type, data_type, key_id} = props
+  variant="outlined", required, helperText, placeholder, multiline=false, more_link_cutoff="", more_link_list_cutoff=props.more_link_cutoff, prevent_edit, input_type, data_type, key_id, openTo="date"} = props
   const [test_state, setTestState] = useState("foo")
   let {with_thumbnail="", with_url="", more_detail=false, toggleMoreDetail} = props
 
@@ -68,6 +68,8 @@ function ACSDate(props) {
                 autoOk
                 required={false}
                 clearable={true}
+                openTo={openTo}
+                views={["year", "month", "date"]}
                 placeholder={placeholder}
                 autoFocus={autoFocus}
                 helperText={helperText}
@@ -110,7 +112,8 @@ function ACSDate(props) {
           <KeyboardDatePicker
             disableToolbar
             autoOk
-            view={["year", "date", "month"]}
+            openTo={openTo}
+            views={["year", "month", "date"]}
             inputProps={{style:{paddingTop:"0px"}}}
             clearable={true}
             variant="inline"
