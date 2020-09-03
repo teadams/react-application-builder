9417d71c1c92d3ca64d587673a60c23c80db41ad
teadams <tracyeadams@gmail.com>
2020-09-02 23:50:25 -0400

    formAttributes

--- src/Functional/Fields/User.js
+++ src/Functional/Fields/User.js
@@ -11,7 +11,7 @@ import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typograp
 , Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar } from '@material-ui/core';
 
 function User(props) {
-  const {row_data, data, field_name, form_field_name, formdata, formAttributes, display, mode="view", field_model={}, image_size,  ...params} = props
+  const {row_data, data, field_name, form_field_name, formdata, formAttributes=[], display, mode="view", field_model={}, image_size,  ...params} = props
   const [formValues, formVisibility, formValidated] = formAttributes
 
   const {with_thumbnail} = field_model
