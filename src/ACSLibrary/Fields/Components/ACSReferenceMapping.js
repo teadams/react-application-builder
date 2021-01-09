25e4ead56901036570ea617b495bec0c0a2c2d0e
Tracy Adams <tracyeadams@gmail.com>
2021-01-09 17:08:48 -0500

    adding checkboxes to reference map

--- src/ACSLibrary/Fields/Components/ACSReferenceMapping.js
+++ src/ACSLibrary/Fields/Components/ACSReferenceMapping.js
@@ -2,21 +2,40 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import * as u from '../../../Utils/utils.js'
 import {ACSListController} from '../../../ACSRenderEngine'
+import {Link, FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField, Select, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar, TableCell,InputLabel,RadioGroup,Radio } from '@material-ui/core';
 
 import React, {Fragment,useEffect} from 'react';
 
 function ACSReferenceMapping(props)  {
-  const {object_type, mode,  field_name, field_list, num_add=3, allow_add=true, allow_save=false,field_model, valid_values, pretty_name,visibility,...params} = props
+  const {object_type, mode,  field_name, field_list, num_add=3, allow_add=true, allow_save=false,field_model, valid_values, pretty_name,visibility,onChange,...params} = props
+
+  function handleChange(event) {
+    const value=event.target.value
+    if (props.onChange) {
+      props.onChange(event)
+    }
+  }
 
   if (["edit","create"].includes(mode)) {
     return (
     <Fragment>
     <div style={{minWidth:"20em", visibility:visibility}}>
       {field_model.summary &&  <div style={{marginBottom:"5px"}} dangerouslySetInnerHTML={{__html: field_model.summary}} 
-  />}
+  />    
+  }
+  <FormGroup row>
     {valid_values!=="transition" && valid_values.map(valid_value =>{
-        return(valid_value.name)
+        const name=field_name+"_acs_"+valid_value.key
+        return(
+               <FormControlLabel
+                 control={<Checkbox checked={false} 
+                onChange={handleChange} 
+                name={name} />}
+                 label={valid_value.name}
+               />
+      )
     })}
+    </FormGroup>
     </div>
     </Fragment>)
   } else {
