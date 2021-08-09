e8d56e71a927c3e6d49f662ef2de921df8b4bbb9
Tracy Adams <tracyeadams@gmail.com>
2021-08-09 16:26:33 -0400

    layout of object and fields

--- src/Custom/ACSAdmin/MetaModelAdmin.js
+++ src/Custom/ACSAdmin/MetaModelAdmin.js
@@ -56,16 +56,18 @@ function MetaModelAdmin(props) {
   }
 
   return (
-  <div>
-   <div style={{width:'200px'}}>  
-   <ACSSelectFilter label="Data Type" key="object_types" onChange={handleObjectTypeFilter} object_type="object_types" filter_name="object_types"  default_value={default_object_value} select_display_field="pretty_plural" any_display_label="-- Select --" select_value_field="key"/>
-   </div>
-   <div style={{width:'200px'}}>
-    <ACSObjectType onData={handleFieldData} headless={true} object_type="fields" />
-    {field_data && selected_object_data &&
-      <ACSSelectFilter data={field_data[selected_object_data.key]} label="Field Name" key="fields" onChange={handleFieldFilter} object_type="fields" filter_name="fields"  default_value={default_field_value} select_display_field="pretty_name" any_display_label="-- Select --" any_item={true} select_value_field="key"/>
-    }
-   </div>
+  <div style={{paddingLeft:"15px"}}>
+    <div style={{display:"flex", flexDirection:"row", alignItems:"flex-start"}}>  
+      <div style={{padding:"15px"}}>  
+        <ACSSelectFilter label="Data Type" key="object_types" onChange={handleObjectTypeFilter} object_type="object_types" filter_name="object_types"  default_value={default_object_value} select_display_field="pretty_plural" any_display_label="-- Select --" select_value_field="key"/>
+      </div>
+      <div style={{padding:"15px"}}>
+        <ACSObjectType onData={handleFieldData} headless={true} object_type="fields" />
+        {field_data && selected_object_data &&
+        <ACSSelectFilter data={field_data[selected_object_data.key]} label="Field Name" key="fields" onChange={handleFieldFilter} object_type="fields" filter_name="fields"  default_value={default_field_value} select_display_field="pretty_name" any_display_label="-- Select --" any_item={true} select_value_field="key"/>
+      }
+      </div>
+    </div>
     <div>
     {selected_object_data && !selected_field_data &&
     <ACSObjectView data={selected_object_data}  object_type="object_types" id={selected_object_data.id}/>
