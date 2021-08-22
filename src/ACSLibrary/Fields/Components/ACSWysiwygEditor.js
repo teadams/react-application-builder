36c249f03c2eea28fef4bd6ef39bce792542d408
Tracy Adams <tracyeadams@gmail.com>
2021-08-22 19:13:28 -0400

    meta

--- src/ACSLibrary/Fields/Components/ACSWysiwygEditor.js
+++ src/ACSLibrary/Fields/Components/ACSWysiwygEditor.js
@@ -5,7 +5,9 @@ import * as u from '../../../Utils/utils.js';
 import * as control from '../../../Utils/control.js';
 
 import { withStyles } from '@material-ui/core/styles';
-import React, { Component, Fragment,  useState, useEffect} from 'react';
+import JoditEditor from "jodit-react";
+
+import React, { Component, Fragment,  useState, useEffect, useRef} from 'react';
 import {Link, OutlinedInput, FormControl, FormHelperText, Input, FormLabel, FormGroup, FormControlLabel, Checkbox, Typography, Chip, Grid, MenuItem, TextField, Select, Dialog, DialogTitle, DialogContent, Divider,DialogContentText, DialogActions, Button, Paper, Avatar, TableCell,InputLabel } from '@material-ui/core';
 import * as meta from '../../../Utils/meta.js';
 import useGetModel from '../../../Hooks/useGetModel.js'
@@ -15,120 +17,70 @@ function ACSWysiwygEditor(props) {
   variant="outlined", required, helperText, placeholder
 } = props
   const [formValues, formVisibility, formValidated] = formAttributes
+  const editor = useRef(null);
   const object_type_model = useGetModel("object_types")[object_type]
 
   let field_value = data[data_field]
-  if (field_value && Object.keys(field_value).length>0) {
-      field_value = JSON.parse(field_value)
-  } else {
-      field_value = ""
- }
 
-const file_url = "";
-let file_exists, file_src, file_name, new_file_name
+ function handleValueChange(new_value) {
+    // simulating the normal event behavior of
+    // a form
+    let event = {};
+    event.target = {};
+    event.target.value = new_value;
+    event.target.name = field_name
+    onChange(event)
+  }
 
-switch (mode) {
+  switch (mode) {
     case "edit":
     case "create":
-      file_exists = false 
-      if (mode === "create" && formValues && Object.keys(formValues).length>0 &&formValues[form_field_name]) {
-        file_exists = true
-        file_src = URL.createObjectURL(formValues[form_field_name])
-        file_name = formValues[form_field_name].name
-      }
-      if (mode === "edit") {
-        const edited_file_name =  formValues[form_field_name].name
-
-        file_name = edited_file_name?edited_file_name:field_value.name
-      }
-      const border_style= {
-        borderColor:"rgba(0, 0, 0, 0.24)",
-        borderStyle: "solid",
-        borderWidth: "1px",
-        display:"flex",
-        flexDirection:"column",
-        padding:"10px",
-        height:"100%",
-        lineHeight:"1.1876em"
+      const value = formValues[form_field_name]
+      const wysiwyg_config = {
+          readonly: false,
+          showTooltip: true,
+          addNewLine: false,
+          placeholder: pretty_name
       }
       return (<Fragment>
-        <div>
-        {field_model.summary &&  <div style={{marginBottom:"5px"}}>{field_model.summary}</div>}
-          <div style={border_style}>
-            {file_name && <div>
-            <Fragment>Filename: {file_name}</Fragment>
-            </div>}
-            <div style={{paddingLeft:"10px"}}>
-               <label htmlFor={form_field_name+"_form"}>
-                {helperText} <br/>
-                 <Button style={{marginTop:"10px"}} color="primary" variant="outlined" component="span">
-                   Upload {pretty_name}
-                 </Button>                
-               </label>    
-               <input
-                 style={{ display: "none" }}
-                 id={form_field_name+"_form"}
-                 name={form_field_name}
-                 key={form_field_name}
-                 type="file"
-                 onChange={onChange}
-               /> 
-            </div>
-            </div>
-          </div>
+        <JoditEditor
+                ref={editor}
+                value={value}
+                config={wysiwyg_config}
+                tabIndex={1} // tabIndex of textarea
+//                onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                onBlur={handleValueChange}
+        />
         </Fragment>
         )
       break
       case "list_create":
       case "list_edit":
-        file_exists = false 
-    //u.a(formValues, object_type, field_value)
-        if (formValues && Object.keys(formValues).length>0 &&formValues[form_field_name]) {
-          file_exists = true
-          file_src = URL.createObjectURL(formValues[form_field_name])
-          new_file_name = formValues[form_field_name].name
-        }
-        file_name = field_value.name
-
         return (<Fragment>
-          <div>
-              {(file_name || new_file_name) &&
-              <Fragment>{new_file_name?new_file_name:file_name} </Fragment>
-              }
-                 <label htmlFor={form_field_name+"_form"}>
-                   <Button size="small" color="primary" variant="outlined" component="span">
-                     Upload
-                   </Button>                
-                 </label>    
-                 <input
-                   style={{ display: "none" }}
-                   id={form_field_name+"_form"}
-                   name={form_field_name}
-                   key={form_field_name}
-                   type="file"
-                   onChange={onChange}
-                 /> 
-            </div>
           </Fragment>
           )
         break
     case "csv":
       if (field_value) {
-        return '"'+ field_value.name+" - " + file_url +'""'
+        return ""
       } else {
         return ""
       }
       break
     case "list":
       if (field_value) {
-        return ( <Fragment><Link name="url_link" key={field_name} id={field_name} target="_blank" rel="noopener" href={file_url}>{field_value.name}</Link> (<Link>edit</Link>)</Fragment>)
+        return ( <Fragment>
+        </Fragment>)
       } else {
         return ""
       }
       break;
     default:
       if (field_value) {
-        return ( <Fragment><Link name="url_link" key={field_name} id={field_name} target="_blank" rel="noopener" href={file_url}>{field_value.name}</Link></Fragment>)
+        return ( <Fragment>
+          <div dangerouslySetInnerHTML={{__html: field_value}} 
+          />
+      </Fragment>)
       } else {
         return ""
       }
