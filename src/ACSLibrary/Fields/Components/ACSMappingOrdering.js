9a0aff0d2b119275afe1cfbafa6f7c0d16305542
Tracy Adams <tracyeadams@gmail.com>
2021-10-15 11:21:40 -0400

    finishing up drag and drop

--- src/ACSLibrary/Fields/Components/ACSMappingOrdering.js
+++ src/ACSLibrary/Fields/Components/ACSMappingOrdering.js
@@ -1,11 +1,13 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
-import * as u from '../../../Utils/utils.js'
+import React, {useContext} from 'react';
+import {usePreviousDistinct} from 'react-use';
 import {Draggable, Droppable} from 'react-beautiful-dnd';
 import styled from 'styled-components'
-import React, {useContext} from 'react';
-import ACSDragDropContext from '../../../Template/ACSDragDropContext.js';
 
+import * as u from '../../../Utils/utils.js'
+
+import ACSDragDropContext from '../../../Template/ACSDragDropContext.js';
 
 const DroppableContainer = styled.div`
   flex-direction:column;
@@ -23,21 +25,30 @@ function ACSMappingOrdering(props)  {
   const {object_type, mode,  field_name, field_list, num_add=3, allow_add=true, data, allow_save=false,field_model, valid_values,
   formAttributes=[],form_field_name=props.field_name, pretty_name,onChange,row_data,...params} = props
 
+
   const drag_drop_context = useContext(ACSDragDropContext)
   let mapping_order = [];
+  data.map((row,index) => {
+   mapping_order.push(row.id)
+  })
+  const string_mapping_order = JSON.stringify(mapping_order);
+  const previous_mapping_order = usePreviousDistinct(mapping_order);
+  const previous_string_mapping_order = JSON.stringify(previous_mapping_order);
 
   if (!data) return null
-
 
   const droppableId = `${field_model.object_type}_###_${field_name}_###_${row_data.id}`
   const drag_key_order = drag_drop_context.drag_key_orders[droppableId]
 
   let ordered_data = []
   // Context has not been set up for this droppable
-  if (!drag_key_order) {
-    data.map((row,index) => {
-     mapping_order.push(row.id)
-    })
+  if (!drag_key_order || 
+      (string_mapping_order !== previous_string_mapping_order &&
+       string_mapping_order !== JSON.stringify(drag_key_order)
+      )) {
+      // new to context OR
+      // data has changed and does not match context
+    u.a("updating")
     drag_drop_context.setKeyOrder(droppableId,mapping_order)
     ordered_data = data;
     return null
